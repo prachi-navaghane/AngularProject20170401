e6e4915a5b8bc29aa729ae654ed09604269c6e2e
prachi-navaghane <prachi.dimble@gmail.com>
2017-04-16 14:27:05 +0530

    directives Hostlistener

--- src/app/directives/sizer.directive.ts
+++ src/app/directives/sizer.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
@@ -11,5 +11,18 @@ export class SizerDirective implements OnInit {
   ngOnInit(): void {
     this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
     // this.elRef.nativeElement.style.width = "500px";
+  }
+  @HostListener('mouseenter')
+  WhenMouseEnters() {
+    //this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
+    this.rend.addClass(this.elRef.nativeElement, 'mixLg');
+    this.rend.removeClass(this.elRef.nativeElement, 'mixMn');
+    console.log(`mouse enter`);
+  }
+  @HostListener('mouseleave')
+  WhenMouseLeaves() {
+    this.rend.removeClass(this.elRef.nativeElement, 'mixLg');
+    this.rend.addClass(this.elRef.nativeElement, 'mixMn');
+   console.log(`mouse leave`);
   }
 }
